4ab90bec4c8be53c5bc4999a5b4624999c79eb22
Malik Abdul Ahad <malikabdulahad01@outlook.com>
2022-12-19 00:22:15 +0500

    emoji_convertor

---
+++ emoji_converter.py
@@ -0,0 +1,13 @@
+message= input('>')
+words=message.split(' ')
+print(words)
+
+emojis  ={
+    ":)" : "🙂",
+    "):": "😔"
+}
+output =""
+for word in words:
+    output +=emojis.get(word, word) + ""
+
+print(output)
