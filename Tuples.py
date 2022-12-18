236a7f1fda77e35260725b14bdf28b4c78c0c06d
Malik Abdul Ahad <malikabdulahad01@outlook.com>
2022-12-18 18:15:49 +0500

    tuples

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+# Tuple is similar to list that can be store a number of list but we can modify more
+#but we can't add new item, we can't remove
+
+numbers = (1, 2, 3, 4)
+print(numbers[0])
+print(numbers[1])
+print(numbers.count(2))
