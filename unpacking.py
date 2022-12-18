9e264daf9f5a911c8a0c84bd5e334f1a570c545a
Malik Abdul Ahad <malikabdulahad01@outlook.com>
2022-12-18 23:04:49 +0500

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,24 @@
+coordinates = (1, 2, 3)
+
+# x= coordinates[0]
+# y= coordinates[1]
+# z= coordinates[2]
+
+x, y, z = coordinates
+
+# lines 3 to 5 and line 7 are same in list and tuples. They do same work
+
+print(x)
+print(y)
+print(z)
+
+print("**************")
+marks = [3, 5, 6]
+
+a, b, c = marks
+
+print(a)
+print(b)
+print(c)
+
+
