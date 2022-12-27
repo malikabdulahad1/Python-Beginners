7915946ccb752e5ead4e3e0dbe52255974335769
Malik Abdul Ahad <malikabdulahad01@outlook.com>
2022-12-27 21:27:01 +0500

    funcations.py

---
+++ funcations.py
@@ -0,0 +1,13 @@
+# function is a container that contains a few lines of code that perform specific task
+
+# print() and input() is a built_in function in python
+
+def greats_user():
+
+    print('welcome to technical encoder')
+
+
+print('start')
+greats_user()
+greats_user()
+print('end')
